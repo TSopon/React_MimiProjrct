ef7d1c211b3ae93aa2d1e69a865fb0c0a60d28e3
TSopon <sopon101n@gmail.com>
2019-12-24 21:20:58 +0700

    add skill

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const Person=({img,name,job})=>{
+const Person=({img,name,job,children})=>{
     const url=  `Http://randomuser.me/api/portraits/thumb/men/${img}.jpg `
     return(
         <article>
             <img src={url}/>
             <h2>Name : {name}</h2>
             <h3>Job  : {job} </h3>
-            
+            {children}
         </article>
     )
 }
@@ -16,8 +16,11 @@ const PeopleList=()=>{
     return(
         <section>
             <Person img="10" name="Sopon" job="Programmer" />
+            <p>Skill : C# Java PHP</p>
             <Person img="25" name="Jonathan Joestar" job="Tester" />
+            <p>Skill : ทดสอบระบบ</p>
             <Person img="6" name="JOJO" job="Designer"/>
+            <p>Skill : Photoshop , 3d model</p>
         </section>
     )
 }
